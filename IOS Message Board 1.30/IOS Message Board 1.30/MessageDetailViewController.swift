92cc8c004b6e8f42e0d39e6a250f99da6dff8328
Sean Doyle <sdoylelambda@gmail.com>
2019-01-30 16:00:09 -0500

    Part 1 Complete - Basic storyboard, skeleton files and outlets.

---
+++ IOS Message Board 1.30/IOS Message Board 1.30/MessageDetailViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func sendClicked(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
