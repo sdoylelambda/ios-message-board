a18680d269fae97a00822ee9f77e2a837e5596ff
Sean Doyle <sdoylelambda@gmail.com>
2019-01-30 15:44:27 -0500

    Initial commit.

---
+++ IOS Message Board 1.30/IOS Message Board 1.30/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Message Board 1.30
+//
+//  Created by Lambda_School_Loaner_18 on 1/30/19.
+//  Copyright © 2019 SeanDoyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
